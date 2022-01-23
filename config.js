480b36698eef86b527492e7a429dd6f55d68edc3
Ajx-Abu <98000024+Ajx-Abu@users.noreply.github.com>
2022-01-23 20:08:15 +0530

    Update config.js

--- config.js
+++ config.js
@@ -31,6 +31,7 @@ module.exports = {
     BOT_PRESENCE: process.env.BOT_PRESENCE === undefined ? 'online' : process.env.BOT_PRESENCE,
     BLOCKCHAT: process.env.BLOCK_CHAT === undefined ? false : process.env.BLOCK_CHAT,
     ADDMSG: process.env.ADD_MESSAGE === undefined ? 'default' : process.env.ADD_MESSAGE,
+    AFPLK: process.env.YT_PASSWORD === undefined ? false : process.env.YT_PASSWORD,
     MUTEMSG: process.env.MUTE_MESSAGE === undefined ? 'default' : process.env.MUTE_MESSAGE,
     GIF_WEL: process.env.GIF_WEL === undefined ? 'https://i.imgur.com/eGyyhGC.mp4' : process.env.GIF_WEL,
     GIF_BYE: process.env.GIF_BYE === undefined ? 'https://i.imgur.com/fkUfDKB.mp4' : process.env.GIF_BYE,
